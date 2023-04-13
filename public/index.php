c9c752c3c0e0378b65a7a02e24e184351161fe05
rayout <freezko@gmail.com>
2023-04-13 18:30:11 +0300

    Рефакторинг роутинга

--- public/index.php
+++ public/index.php
@@ -5,8 +5,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Controllers\Controller;
 use App\Core\Application;
 
+// нужно вынести в отдельный файл хэлперов
+function dd()
+{
+    echo '<pre>';
+    $args = func_get_args();
+    call_user_func_array('var_dump', $args);
+    die();
+}
+
 $app = new Application();
-$app->router->get('/^\/{id}\/{aba}$/', [Controller::class, 'index']);
+$app->router->get('/document/{id}', [Controller::class, 'index']);
 $app->router->get('/^\/caba\/ca[xds]a$/', [Controller::class, 'show']);
 $app->run();
 
