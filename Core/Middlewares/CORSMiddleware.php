fb0100cb9d1b22ee0144fce755ee6cc9931cb6a1
maximrakov <maximrakov2210@mail.ru>
2023-04-21 17:28:44 +0300

    fix: CORS middleware

--- Core/Middlewares/CORSMiddleware.php
+++ Core/Middlewares/CORSMiddleware.php
@@ -7,14 +7,9 @@ use App\Core\Response;
 
 class CORSMiddleware implements Middleware
 {
-    private $allowedHosts = [];
-
     public function handle(Request $request, Response $response)
     {
-        $requestHost = $_SERVER['REMOTE_ADDR'] . ':' . $_SERVER['REMOTE_PORT'];
-        if (!in_array($requestHost, $this->allowedHosts)) {
-            $response->setStatusCode(403);
-            return false;
-        }
+        $response->header('access-control-allow-origin', '*');
+        return $request;
     }
 }
